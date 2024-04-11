a65c64f390434bee98c416750ca94594cfdcb483
Tijl Moreau <moreautijl@gmail.com>
2024-04-11 17:49:38 +0200

    Modified trades to be less expensive.

--- src/main/java/net/micaxs/smokeleafindustry/event/ModEvents.java
+++ src/main/java/net/micaxs/smokeleafindustry/event/ModEvents.java
@@ -27,43 +27,43 @@ public class ModEvents {
 
             // Level 1 Trades
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 2),
+                    new ItemStack(Items.IRON_INGOT, 2),
                     new ItemStack(ModItems.WHITE_WIDOW_BAG.get(), 1),
                     64, 3, 0.02f));
 
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 5),
+                    new ItemStack(Items.IRON_INGOT, 3),
                     new ItemStack(ModItems.BUBBLE_KUSH_BAG.get(), 1),
                     64, 3, 0.02f));
 
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 8),
+                    new ItemStack(Items.GOLD_INGOT, 2),
                     new ItemStack(ModItems.LEMON_HAZE_BAG.get(), 1),
                     64, 3, 0.02f));
 
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 12),
+                    new ItemStack(Items.GOLD_INGOT, 3),
                     new ItemStack(ModItems.SOUR_DIESEL_BAG.get(), 1),
                     64, 3, 0.02f));
 
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 16),
+                    new ItemStack(Items.DIAMOND, 2),
                     new ItemStack(ModItems.BLUE_ICE_BAG.get(), 1),
                     64, 3, 0.02f));
 
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 20),
+                    new ItemStack(Items.DIAMOND, 3),
                     new ItemStack(ModItems.BUBBLEGUM_BAG.get(), 1),
+                    64, 3, 0.02f));
+
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.NETHERITE_INGOT, 1),
+                    new ItemStack(ModItems.PURPLE_HAZE_BAG.get(), 1),
                     64, 3, 0.02f));
 
             // Level 2 Trades
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 8),
-                    new ItemStack(ModItems.LEMON_HAZE_BAG.get(), 1),
-                    64, 3, 0.02f));
-
-            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 12),
+                    new ItemStack(Items.GOLD_INGOT, 3),
                     new ItemStack(ModItems.SOUR_DIESEL_BAG.get(), 1),
                     64, 3, 0.02f));
 
@@ -77,12 +77,12 @@ public class ModEvents {
                     new ItemStack(ModItems.BUBBLEGUM_BAG.get(), 1),
                     64, 3, 0.02f));
 
-            // Level 3 Trades
-            trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 12),
-                    new ItemStack(ModItems.SOUR_DIESEL_BAG.get(), 1),
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.NETHERITE_INGOT, 1),
+                    new ItemStack(ModItems.PURPLE_HAZE_BAG.get(), 1),
                     64, 3, 0.02f));
 
+            // Level 3 Trades
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.DIAMOND, 16),
                     new ItemStack(ModItems.BLUE_ICE_BAG.get(), 1),
@@ -102,6 +102,11 @@ public class ModEvents {
             trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
                     new ItemStack(Items.DIAMOND, 20),
                     new ItemStack(ModItems.BUBBLEGUM_BAG.get(), 1),
+                    64, 3, 0.02f));
+
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.NETHERITE_INGOT, 1),
+                    new ItemStack(ModItems.PURPLE_HAZE_BAG.get(), 1),
                     64, 3, 0.02f));
 
         }
