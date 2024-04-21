e8a59b345698f61802b619b8f57bbc847374499a
Tijl Moreau <moreautijl@gmail.com>
2024-04-21 20:34:40 +0200

    Reworked BaseWeedCropBlock
    This was a pain to have it work with Harvest with ease mod and its probably a bad way, but at least it works.

--- src/main/java/net/micaxs/smokeleafindustry/block/custom/BaseWeedCropBlock.java
+++ src/main/java/net/micaxs/smokeleafindustry/block/custom/BaseWeedCropBlock.java
@@ -5,7 +5,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.*;
@@ -14,6 +16,8 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -36,6 +40,9 @@ public class BaseWeedCropBlock extends CropBlock {
     public int THC = 0;
     public int CBD = 0;
 
+    public static final IntegerProperty AGE = IntegerProperty.create("age",0, 10);
+    public static final IntegerProperty TEXTURE_AGE = IntegerProperty.create("texture_age",0, 10);
+
     private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
             Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0),
             Block.box(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
@@ -51,8 +58,6 @@ public class BaseWeedCropBlock extends CropBlock {
             Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 16.0)
     };
 
-
-    public static final IntegerProperty AGE = IntegerProperty.create("age",0, 10);
 
     public BaseWeedCropBlock(Properties pProperties, Supplier<Item> seedItem, Supplier<Item> budItem) {
         super(pProperties);
@@ -74,6 +79,7 @@ public class BaseWeedCropBlock extends CropBlock {
         return seedItem.get();
     }
 
+
     @Override
     public @NotNull VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE_BY_AGE[this.getAge(pState)];
@@ -85,36 +91,59 @@ public class BaseWeedCropBlock extends CropBlock {
     }
 
 
+    @Override
     public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
         if (!pLevel.isAreaLoaded(pPos, 1)) return;
 
         if (pLevel.getRawBrightness(pPos, 0) >= 11) {
             int currentAge = this.getAge(pState);
+            int currentTextureAge = this.getTextureAge(pState);
+            BlockPos abovePos = pPos.above(1);
+            BlockState aboveBlockState = pLevel.getBlockState(abovePos);
 
-            if (currentAge < this.getMaxAge()) {
-                float growthSpeed = getGrowthSpeed(this, pLevel, pPos);
+            if (pLevel.getBlockState(pPos.below(1)).is(this)) {
+                return;
+            }
 
-                if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(pLevel, pPos, pState, pRandom.nextInt((int)(25.0F / growthSpeed) + 1) == 0)) {
-
-                    BlockPos abovePos = pPos.above(1);
-
-                    if (currentAge == FIRST_STAGE_MAX_AGE) {
-                        if (pLevel.getBlockState(abovePos).is(Blocks.AIR)) {
-                            pLevel.setBlock(abovePos, this.getStateForAge(currentAge + 1), 2);
-                        } else if (pLevel.getBlockState(abovePos).is(this)) {
-                            // get the age of the block above and set it to +1
-                            BlockState aboveBlockState = pLevel.getBlockState(abovePos);
-                            int aboveBlockAge = this.getAge(aboveBlockState);
-                            if (aboveBlockAge < this.getMaxAge()) {
-                                pLevel.setBlock(abovePos, this.getStateForAge(aboveBlockAge + 1), 2);
-                            }
-                        }
-                    } else {
-                        pLevel.setBlock(pPos, this.getStateForAge(currentAge + 1), 2);
+            if (currentAge < this.getMaxAge() && currentTextureAge < this.getMaxAge()) {
+                if (currentAge < FIRST_STAGE_MAX_AGE) {
+                    if (aboveBlockState.is(Blocks.AIR)) {
+                        pLevel.setBlock(pPos, this.getStateForAge(currentAge + 1).setValue(AGE, currentAge + 1).setValue(TEXTURE_AGE, currentAge + 1), 2);
                     }
-
-                    net.minecraftforge.common.ForgeHooks.onCropsGrowPost(pLevel, pPos, pState);
+                } else {
+                    if (aboveBlockState.is(Blocks.AIR)) {
+                        pLevel.setBlock(pPos, this.getStateForAge(currentAge + 1).setValue(AGE, currentAge + 1).setValue(TEXTURE_AGE, FIRST_STAGE_MAX_AGE), 2);
+                        pLevel.setBlock(abovePos, this.getStateForTextureAge(currentAge + 1).setValue(TEXTURE_AGE, currentAge + 1), 2);
+                    } else if (aboveBlockState.is(this) && this.getTextureAge(aboveBlockState) < this.getMaxAge()) {
+                        pLevel.setBlock(pPos, this.getStateForAge(currentAge + 1).setValue(AGE, currentAge + 1).setValue(TEXTURE_AGE, FIRST_STAGE_MAX_AGE), 2);
+                        pLevel.setBlock(abovePos, this.getStateForTextureAge(currentAge + 1).setValue(TEXTURE_AGE, currentAge + 1), 2);
+                    }
                 }
+            }
+        }
+    }
+
+    @Override
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        super.entityInside(pState, pLevel, pPos, pEntity);
+
+        if (!(pLevel instanceof ServerLevel)) {
+            return;
+        }
+
+        ServerLevel serverWorld = (ServerLevel) pLevel;
+
+        int currentAge = this.getAge(pState);
+        int currentTextureAge = this.getTextureAge(pState);
+        BlockPos abovePos = pPos.above();
+        BlockState aboveBlockState = pLevel.getBlockState(abovePos);
+
+        // If the current age is less than FIRST_STAGE_MAX_AGE and there's no crop above this one
+        if (currentAge < FIRST_STAGE_MAX_AGE && !aboveBlockState.is(this)) {
+            // If the texture age is not the same as the current age
+            if (currentTextureAge != currentAge) {
+                // Set the texture age to be the same as the current age
+                serverWorld.setBlock(pPos, pState.setValue(TEXTURE_AGE, currentAge), 2);
             }
         }
     }
@@ -124,7 +153,7 @@ public class BaseWeedCropBlock extends CropBlock {
     public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
         return super.canSurvive(pState, pLevel, pPos) ||
                 (pLevel.getBlockState(pPos.below(1)).is(this) &&
-                        pLevel.getBlockState(pPos.below(1)).getValue(AGE) == 7);
+                        pLevel.getBlockState(pPos.below(1)).getValue(AGE) >= 7);
     }
 
     @Override
@@ -132,44 +161,44 @@ public class BaseWeedCropBlock extends CropBlock {
         int nextAge = this.getAge(pState) + this.getBonemealAgeIncrease(pLevel);
         int maxAge = this.getMaxAge();
         int currentAge = this.getAge(pState);
+        BlockPos belowPos = pPos.below(1);
 
         if (nextAge > maxAge) {
             nextAge = maxAge;
         }
 
-        BlockPos belowPos = pPos.below(1);
         if (!pLevel.getBlockState(belowPos).is(this)) {
-            if (currentAge < FIRST_STAGE_MAX_AGE) {
+            if (currentAge <= FIRST_STAGE_MAX_AGE) { // Changed from < to <=
                 if (nextAge > FIRST_STAGE_MAX_AGE) {
                     BlockPos abovePos = pPos.above(1);
                     if (pLevel.getBlockState(abovePos).is(Blocks.AIR)) {
-                        pLevel.setBlock(pPos, this.getStateForAge(FIRST_STAGE_MAX_AGE), 2);
-                        pLevel.setBlock(abovePos, this.getStateForAge(nextAge), 2);
+                        pLevel.setBlock(pPos, this.getStateForAge(FIRST_STAGE_MAX_AGE).setValue(AGE, nextAge).setValue(TEXTURE_AGE, FIRST_STAGE_MAX_AGE), 2);
+                        pLevel.setBlock(abovePos, this.getStateForTextureAge(nextAge).setValue(TEXTURE_AGE, nextAge), 2);
+                    } else if (pLevel.getBlockState(abovePos).is(this) && this.getTextureAge(pLevel.getBlockState(abovePos)) < this.getMaxAge()) {
+                        pLevel.setBlock(pPos, this.getStateForAge(FIRST_STAGE_MAX_AGE).setValue(AGE, nextAge).setValue(TEXTURE_AGE, FIRST_STAGE_MAX_AGE), 2);
+                        pLevel.setBlock(abovePos, this.getStateForTextureAge(nextAge).setValue(TEXTURE_AGE, nextAge), 2);
                     }
                 } else {
-                    pLevel.setBlock(pPos, this.getStateForAge(nextAge), 2);
+                    pLevel.setBlock(pPos, this.getStateForAge(nextAge).setValue(AGE, nextAge).setValue(TEXTURE_AGE, nextAge), 2);
                 }
             } else {
-                if (nextAge < maxAge + 1) {
-                    BlockPos abovePos = pPos.above(1);
-                    BlockState aboveBlockState = pLevel.getBlockState(abovePos);
-
-                    if (aboveBlockState.is(this)) {
-                        int aboveBlockAge = this.getAge(aboveBlockState);
-
-                        if (aboveBlockAge < maxAge) {
-                            pLevel.setBlock(pPos.above(1), this.getStateForAge(nextAge), 2);
-                        } else if (aboveBlockAge == maxAge) {
-                            pLevel.destroyBlock(pPos, true);
-                            // Maybe replant the first stage here?
-                            pLevel.setBlock(pPos, this.getStateForAge(0), 2);
-                        }
-                    } else if (aboveBlockState.is(Blocks.AIR)) {
-                        pLevel.setBlock(pPos.above(1), this.getStateForAge(nextAge), 2);
-                    }
+                pLevel.setBlock(pPos, this.getStateForAge(nextAge).setValue(AGE, nextAge).setValue(TEXTURE_AGE, FIRST_STAGE_MAX_AGE), 2);
+                BlockPos abovePos = pPos.above(1);
+                if (pLevel.getBlockState(abovePos).is(this) && this.getTextureAge(pLevel.getBlockState(abovePos)) < this.getMaxAge()) {
+                    pLevel.setBlock(abovePos, this.getStateForTextureAge(nextAge).setValue(TEXTURE_AGE, nextAge), 2);
                 }
             }
         }
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader pLevel, BlockPos pPos, BlockState pState, boolean pIsClient) {
+        BlockPos belowPos = pPos.below();
+        BlockState belowState = pLevel.getBlockState(belowPos);
+        if (belowState.getBlock() instanceof BaseWeedCropBlock && ((BaseWeedCropBlock) belowState.getBlock()).getAge(belowState) > 7) {
+            return false;
+        }
+        return super.isValidBonemealTarget(pLevel, pPos, pState, pIsClient);
     }
 
 
@@ -229,32 +258,36 @@ public class BaseWeedCropBlock extends CropBlock {
             return;
         }
 
-        BlockPos belowPos = pPos.below();
-        BlockState belowBlockState = level.getBlockState(belowPos);
-        BlockPos abovePos = pPos.above();
-        BlockState aboveBlockState = level.getBlockState(abovePos);
-        if (!belowBlockState.is(this)) {
-            if (this.getAge(pState) < 7) {
-                this.dropSeedItem(level, pPos, pState);
-                return;
-            } else if (this.getAge(pState) == FIRST_STAGE_MAX_AGE) {
-                if (aboveBlockState.is(this) && this.getAge(aboveBlockState) == this.getMaxAge()) {
-                    return;
-                } else {
-                    this.dropSeedItem(level, pPos, pState);
-                    return;
-                }
-            }
+        if (this.getAge(pState) >= this.getMaxAge()) {
+            this.dropFullLoot(level, pPos, pState);
         } else {
-            if (this.getAge(pState) == this.getMaxAge()) {
-                this.dropFullLoot(level, belowPos, pState);
-                level.destroyBlock(belowPos, false);
-            } else {
-                this.dropSeedItem(level, belowPos, pState);
-                level.destroyBlock(belowPos, false);
-            }
+            this.dropSeedItem(level, pPos, pState);
         }
+
         super.destroy(level, pPos, pState);
+    }
+
+
+
+    public int getTextureAge(BlockState pState) {
+        return pState.getValue(TEXTURE_AGE);
+    }
+
+    protected IntegerProperty getTextureAgeProperty() {
+        return TEXTURE_AGE;
+    }
+
+    public BlockState getStateForTextureAge(int pAge) {
+        return this.defaultBlockState().setValue(this.getTextureAgeProperty(), pAge);
+    }
+
+    @Override
+    public @NotNull BlockState getStateForAge(int age) {
+        BlockState state = super.getStateForAge(age);
+        if (age < FIRST_STAGE_MAX_AGE) {
+            return state.setValue(TEXTURE_AGE, age);
+        }
+        return state;
     }
 
     @Override
@@ -274,6 +307,6 @@ public class BaseWeedCropBlock extends CropBlock {
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(AGE);
+        pBuilder.add(AGE, TEXTURE_AGE);
     }
 }
