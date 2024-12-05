6e9efe491133747214f73e21868e00822a9a2ba2
Asis <47801367+Asis2019@users.noreply.github.com>
2024-12-05 08:56:22 +0200

    Removed duration from NBT tags

--- src/main/java/net/micaxs/smokeleafindustry/utils/HashOilHelper.java
+++ src/main/java/net/micaxs/smokeleafindustry/utils/HashOilHelper.java
@@ -1,13 +1,13 @@
 package net.micaxs.smokeleafindustry.utils;
 
-import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import net.micaxs.smokeleafindustry.SmokeleafIndustryMod;
 import net.micaxs.smokeleafindustry.item.custom.BaseWeedItem;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
@@ -16,7 +16,6 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class HashOilHelper {
     public static boolean setWeedNBTDataFluid(ItemStack itemHandler, FluidStack weedDerivedFluid) {
@@ -29,25 +28,20 @@ public class HashOilHelper {
 
         // Check if oil already has active ingredients
         String previousData = fluidTag.getString("active_ingredients");
-        JsonObject activeIngredientsTag = new JsonObject();
+        JsonArray activeIngredientsList = new JsonArray();
         if (!previousData.equals("")) {
-            activeIngredientsTag = JsonParser.parseString(fluidTag.getString("active_ingredients")).getAsJsonObject();
+            activeIngredientsList = JsonParser.parseString(fluidTag.getString("active_ingredients")).getAsJsonArray();
         }
 
         // Check if 2 active ingredients are already in the set and exit if it is.
-        if (activeIngredientsTag.keySet().size() >= 2) {
+        if (activeIngredientsList.size() >= 2) {
             return true;
         }
 
-        // Set duration tag
-        // TODO: duration is stored in the key, doesn't need to be here
-        if (weedItem.getEffect() == MobEffects.LEVITATION) {
-            activeIngredientsTag.addProperty(weedItem.getDescriptionId(), 200);
-        } else {
-            activeIngredientsTag.addProperty(weedItem.getDescriptionId(), 400);
-        }
+        // Add active ingredient to list
+        activeIngredientsList.add(weedItem.getDescriptionId());
 
-        fluidTag.putString("active_ingredients", activeIngredientsTag.toString());
+        fluidTag.putString("active_ingredients", activeIngredientsList.toString());
         weedDerivedFluid.setTag(fluidTag);
         return false;
     }
@@ -90,17 +84,16 @@ public class HashOilHelper {
         }
 
         // extract the active ingredients
-        JsonObject activeIngredientsTag = JsonParser.parseString(tag.getString("active_ingredients")).getAsJsonObject();
-        Set<String> activeIngredients = activeIngredientsTag.keySet();
-        if (activeIngredients.size() >= 3) {
+        JsonArray activeIngredientsTag = JsonParser.parseString(tag.getString("active_ingredients")).getAsJsonArray();
+        if (activeIngredientsTag.size() >= 3) {
             // Blend is "dead" if more than 3 types are mixed
             return List.of();
         }
 
         List<BaseWeedItem> result = new ArrayList<>();
-        for (String activeIngredientName : activeIngredients) {
-            String[] parts = activeIngredientName.split("\\.");
-            activeIngredientName = parts[parts.length - 1];
+        for (JsonElement activeIngredientElement : activeIngredientsTag.asList()) {
+            String[] parts = activeIngredientElement.getAsString().split("\\.");
+            String activeIngredientName = parts[parts.length - 1];
             RegistryObject<Item> activeIngredient = RegistryObject.create(new ResourceLocation(SmokeleafIndustryMod.MOD_ID, activeIngredientName), ForgeRegistries.ITEMS);
             if (activeIngredient.get() instanceof BaseWeedItem activeIngredientItem) {
                 activeIngredientItem.setVariableDuration(false);
