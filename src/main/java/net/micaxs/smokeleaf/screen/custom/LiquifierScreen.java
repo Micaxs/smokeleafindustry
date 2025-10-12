8434e319f6c083d1fc1d8ef106c49899c845e2d9
Tijl <moreautijl@gmail.com>
2025-10-12 19:58:10 +0200

    Fixed labels and position of tooltips inside the screens.

--- src/main/java/net/micaxs/smokeleaf/screen/custom/LiquifierScreen.java
+++ src/main/java/net/micaxs/smokeleaf/screen/custom/LiquifierScreen.java
@@ -12,6 +12,7 @@ import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.neoforge.fluids.FluidStack;
@@ -56,15 +57,6 @@ public class LiquifierScreen extends AbstractContainerScreen<LiquifierMenu> {
 
 
     @Override
-    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-        int x = (width - imageWidth) / 2;
-        int y = (height - imageHeight) / 2;
-        renderEnergyInfoArea(guiGraphics, mouseX, mouseY, x, y);
-        renderFluidTooltipArea(guiGraphics, mouseX, mouseY, x, y, menu.blockEntity.getFluid(), 134, 11, fluidRenderer);
-        renderInfoIconTooltip(guiGraphics, mouseX, mouseY, x, y);
-    }
-
-    @Override
     protected void renderBg(GuiGraphics guiGraphics, float v, int i, int i1) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
@@ -88,16 +80,9 @@ public class LiquifierScreen extends AbstractContainerScreen<LiquifierMenu> {
 
     private void renderInfoIconTooltip(GuiGraphics g, int mouseX, int mouseY, int baseX, int baseY) {
         if (isMouseAboveAreaEnergy(mouseX, mouseY, baseX, baseY, ICON_SIZE, ICON_SIZE, ICON_SIZE, ICON_SIZE)) {
-            g.pose().pushPose();
-            g.pose().scale(TOOLTIP_SCALE, TOOLTIP_SCALE, 1.0f);
-            g.renderTooltip(
-                    this.font,
-                    List.of(Component.translatable("gui.tooltip.liquifier.info")),
-                    Optional.empty(),
-                    (mouseX / 2) - (baseX / 4),
-                    mouseY / 2
-            );
-            g.pose().popPose();
+            Component info = Component.translatable("gui.tooltip.liquifier.info");
+            List<FormattedCharSequence> wrapped = this.font.split(info, 300);
+            g.renderTooltip(this.font, wrapped, mouseX, mouseY);
         }
     }
 
@@ -160,8 +145,21 @@ public class LiquifierScreen extends AbstractContainerScreen<LiquifierMenu> {
     }
 
     @Override
+    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+        renderEnergyInfoArea(guiGraphics, mouseX, mouseY, x, y);
+        renderFluidTooltipArea(guiGraphics, mouseX, mouseY, x, y, menu.blockEntity.getFluid(), 134, 11, fluidRenderer);
+    }
+
+    @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         super.render(guiGraphics, mouseX, mouseY, partialTick);
+
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+        renderInfoIconTooltip(guiGraphics, mouseX, mouseY, x, y);
+
         renderTooltip(guiGraphics, mouseX, mouseY);
     }
 }
--- src/main/java/net/micaxs/smokeleaf/screen/custom/SequencerScreen.java
+++ src/main/java/net/micaxs/smokeleaf/screen/custom/SequencerScreen.java
@@ -9,6 +9,7 @@ import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.player.Inventory;
 
 import java.util.List;
@@ -39,14 +40,6 @@ public class SequencerScreen extends AbstractContainerScreen<SequencerMenu> {
 
 
     @Override
-    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-        int x = (width - imageWidth) / 2;
-        int y = (height - imageHeight) / 2;
-        renderEnergyInfoArea(guiGraphics, mouseX, mouseY, x, y);
-        renderInfoIconTooltip(guiGraphics, mouseX, mouseY, x, y);
-    }
-
-    @Override
     protected void renderBg(GuiGraphics guiGraphics, float v, int i, int i1) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
@@ -71,16 +64,9 @@ public class SequencerScreen extends AbstractContainerScreen<SequencerMenu> {
 
     private void renderInfoIconTooltip(GuiGraphics g, int mouseX, int mouseY, int baseX, int baseY) {
         if (isMouseAboveArea(mouseX, mouseY, baseX, baseY, ICON_SIZE, ICON_SIZE, ICON_SIZE, ICON_SIZE)) {
-            g.pose().pushPose();
-            g.pose().scale(TOOLTIP_SCALE, TOOLTIP_SCALE, 1.0f);
-            g.renderTooltip(
-                    this.font,
-                    List.of(Component.translatable("gui.tooltip.sequencer.info")),
-                    Optional.empty(),
-                    (mouseX / 2) - (baseX / 4),
-                    mouseY / 2
-            );
-            g.pose().popPose();
+            Component info = Component.translatable("gui.tooltip.sequencer.info");
+            List<FormattedCharSequence> wrapped = this.font.split(info, 300);
+            g.renderTooltip(this.font, wrapped, mouseX, mouseY);
         }
     }
 
@@ -107,8 +93,18 @@ public class SequencerScreen extends AbstractContainerScreen<SequencerMenu> {
     }
 
     @Override
+    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+        renderEnergyInfoArea(guiGraphics, mouseX, mouseY, x, y);
+    }
+
+    @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         super.render(guiGraphics, mouseX, mouseY, partialTick);
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+        renderInfoIconTooltip(guiGraphics, mouseX, mouseY, x, y);
         renderTooltip(guiGraphics, mouseX, mouseY);
     }
 }
